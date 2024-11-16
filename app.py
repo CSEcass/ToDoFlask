34f3fb03cdf70e08849aa0bbbf2b58270a1698ac
Cassidy <cassidy.reyes@csedge.org>
2024-11-16 14:42:04 -0800

    made base html starter code

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+from flask import Flask, render_template, request
 from tinydb import TinyDB, Query
-# Creating an instance VVV
-db = TinyDB('./db.json')
 
-todo = Query()
+db = TinyDB('./db.json')
+app = Flask(__name__)
+@app.route('/', methods=['GET', 'POST'])
+def hello():
+    todoName = request.form.get('todo')
+    if todoName == '':
+        print('No Input.')
+    else:
+        db.insert({'Name': todoName})
+    Name = todoName
+    return render_template('index.html', Name=Name)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
