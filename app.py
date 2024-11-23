43b08f4dd3c48ea86886bd6b22c5eb605f3e1d19
Cassidy <cassidy.reyes@csedge.org>
2024-11-23 14:57:04 -0800

    FINISHED ADDING AND DELETING FUNCTION, UPDATE STILL A WIP

--- app.py
+++ app.py
@@ -1,17 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from tinydb import TinyDB, Query
 
-db = TinyDB('./db.json')
+db = TinyDB('db.json')
 app = Flask(__name__)
+todo = Query()
+    
 @app.route('/', methods=['GET', 'POST'])
 def hello():
-    todoName = request.form.get('todoCre')
-    if todoName == '':
-        print('No Input.')
-    else:
-        db.insert({'Name': todoName})
-    Name = todoName
-    return render_template('index.html', Name=Name)
+    if request.method == 'POST':
+        Name = request.form.get('todoName')
+        Desc = request.form.get('todoDesc')
+        db.insert({'Name': Name,'Desc': Desc})
+    todos = db.all()
+    return render_template('index.html', todos=todos)
+@app.route('/dele', methods=['GET', 'POST'])
+def delete():
+    if request.method == 'POST':
+        todoDel = request.form.get('todoDel')
+        db.remove(todo.Name == todoDel)
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='5000')
